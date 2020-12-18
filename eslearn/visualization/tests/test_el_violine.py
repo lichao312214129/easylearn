d6fff4513c6517e40e251300920ebd6dbef6387c
lichao312214129 <lichao19870617@gmail.com>
2020-12-18 20:59:44 +0800

    1.0.8beta

--- eslearn/visualization/tests/test_el_violine.py
+++ eslearn/visualization/tests/test_el_violine.py
@@ -1,14 +1,23 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from eslearn.visualization.el_violine import ViolinPlotMatplotlib
+import mplcyberpunk
+from eslearn.visualization.el_violine import ViolinPlotMatplotlib, ViolinPlot
 
 data = pd.read_excel("./violin_demo_data.xlsx")
 var_name = list(data.columns)
 
 data_list = list(data.values.T)
-plt.figure(figsize=(5,5))
-ViolinPlotMatplotlib().plot(data_list, positions=np.arange(0,len(data_list)), facecolor=['r', 'g', 'b'])
-plt.xticks(np.arange(0,len(data_list)), var_name, rotation=45)
+
+ViolinPlotMatplotlib().plot(data_list, 
+                            positions=np.arange(0,len(data_list)))
+
+# ViolinPlot().plot(data_list)
+
+
+plt.xticks(np.arange(0,len(data_list)), var_name, rotation=45, color='w', fontsize=15)
 plt.tight_layout()
+mplcyberpunk.add_underglow()
+plt.show()
+
 plt.savefig("violin.pdf")
